4387ad4c0327c7c07323554263a1543713eab4f4
pooja solpaurmath <Pooja.solapurmaththeknowledgeacademy.com>
2024-01-04 17:52:40 +0530

    edit

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -1,20 +1,24 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $projects = project::all();
         return view('project.list', compact('projects'));
     }
-    public function create(){
+    public function create()
+    {
 
         return view('project.create');
     }
-    public function store(Request $request){
+    public function store(Request $request)
+    {
 
 
         project::create([
@@ -24,5 +28,31 @@ class ProjectController extends Controller
 
         session()->flash('success', 'project Created successfully.');
         return redirect()->route('project.index');
+    }
+    public function edit(project $project)
+    {
+        // echo"fdgg";
+        $project = project::with('creator')->find($project->id);
+
+        return view('project.edit', compact('project'));
+    }
+    public function update(Request $request, project $project)
+    {
+        $project->update([
+            'name' => $request->name,
+            // Add any other fields you need to update
+        ]);
+
+        session()->flash('success', 'project updated successfully.');
+        return redirect()->route('project.index');
+    }
+
+    // /**
+    //  * Remove the specified resource from storage.
+    //  */
+    public function destroy(project $project)
+    {
+        $project->delete();
+        return redirect()->route('project.index')->with('success', 'project deleted successfully.');
     }
 }
